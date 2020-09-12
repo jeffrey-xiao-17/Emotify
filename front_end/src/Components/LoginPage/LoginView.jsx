ba0009a823cbb98fbbcb7383e6b321bd3714d84b
Luis Sangueado <luissangueado@MacBook-Pro.local>
2020-09-11 21:54:30 -0700

    fixed bugs

--- front_end/src/Components/LoginPage/LoginView.jsx
+++ front_end/src/Components/LoginPage/LoginView.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ".../css/LoginView.css"
+import styles from "../../css/LoginView.module.css";
 import SampleButton from './components/SampleButton'
 
 function LoginView() {
   return (
-    <div className="LoginView">
-      <h1 className="titleText">GAME TITLE</h1>
+    <div className={styles.LoginView}>
+      <h1 className={styles.titleText}>GAME TITLE</h1>
 
-      <div className = "buttonContainer"> 
-        <SampleButton buttonText= {"Log in with Google"}/>
-        <SampleButton buttonText= {"Exit"}/>
+      <div className = {styles.buttonContainer}> 
+        <button className= {styles.button}>Login with Google</button>
+        <button className= {styles.button}>Exit</button>
       </div>
     </div>
   );
