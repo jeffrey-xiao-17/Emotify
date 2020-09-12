bc5fe0be44d6f32fbf74b6d520a13203d583e5b7
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-09-11 20:11:40 -0400

    index and basic app.js/view

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./App.module.css";
+
+// Components
+import View from "./Components/View/View";
+
+function App() {
+  return (
+    <div className={styles.center}>
+      <View />
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end/src/Components/View/View.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class View extends Component {
+  render() {
+    return <div>View</div>;
+  }
+}
+
+export default View;
