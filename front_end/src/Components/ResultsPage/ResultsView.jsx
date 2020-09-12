7751a31da4ec7674757cfdbc5cae34d07798341a
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-09-11 23:22:17 -0400

    updated nav bar to be responsive

---
+++ front_end/src/Components/ResultsPage/ResultsView.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+// import styles from "../../css/InteractionView.module.css";
+import cx from "classnames";
+
+class ResultsView extends Component {
+  render() {
+    return <div>Results</div>;
+  }
+}
+
+export default ResultsView;
