5507490cdeb429de728c61eabe8a1ca4c2e09fcb
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-09-13 06:25:48 -0400

    added nlp feature to moodboost

--- front_end/src/Components/EmpathizePage/empathize.js
+++ front_end/src/Components/EmpathizePage/empathize.js
@@ -1,18 +1,21 @@
 import * as Util from "../../utilities";
 
-import axios from 'axios';
+import axios from "axios";
 
 const ANALYZE_ENTITY_SENTIMENT_METHOD =
   "https://language.googleapis.com/v1/documents:analyzeEntitySentiment";
-
 
 const ACCESS_TOKEN =
   "ya29.c.Ko8B3Afv9eriAEJnvg-riokaH7tz3KKGhpxwy6sEoDTQae8Xi3PYa-iHCCAUGDZWMp8xRphOvxaoIAp7vEG50G9jU-O4hVdtUlDXyPk1GwTPOn1HClBAQXk66ZbE6a4RBJ1tc2TQfxUb22pBHnvDRZasG5nRv7OpHvpiYwRxPns1UuNfXGjPEs36LhpRcR4Dvuc";
 
 export async function analyzeText(text) {
-  const response = await axios.post('https://autismproject.uc.r.appspot.com/process', {
-    text
-  });
+  const response = await axios.post(
+    "https://autismproject.uc.r.appspot.com/process",
+    {
+      text,
+    }
+  );
+  console.log(response.data);
   return response.data;
 }
 
@@ -28,5 +31,5 @@ const SAMPLES = [
     "I HATE GOOGLE I HATE GOOGLE SO MUCH PLEASE LET US LOSE!!!",
     "WFH vs office",
     "https://news.ycombinator.com/item?id=24452280",
-  ]
+  ],
 ];
