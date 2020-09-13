bfab8d2b06116da08088c870af49438caba1b619
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-09-13 04:50:08 -0400

    results

--- front_end/src/Components/ResultsPage/ResultsView.jsx
+++ front_end/src/Components/ResultsPage/ResultsView.jsx
@@ -3,9 +3,81 @@ import styles from "../../css/Results.module.css";
 import { Grid, Message, Statistic } from "semantic-ui-react";
 
 class ResultsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userScore: props.userScore,
+      simScore: props.simScore,
+    };
+  }
+
+  tipsAndTakeaways(diff) {
+    if (diff <= -1) {
+      return [
+        "If possible, try to express positive emotions and relating to your partner's facial expression.",
+        'Words like "enjoy," "awesome," and "ecstatic" are strong indicators of positive emotion.',
+      ];
+    } else if (diff <= -0.5) {
+      return [
+        "Keep it up! Understanding others at first glance is not easy - it takes time and practice!",
+        "Listen to your partner when he/she talks. They are telling you more than just words!",
+      ];
+    } else if (diff <= 0) {
+      return [
+        "You're have a great grasp of your partner's emotional situation!",
+        'Try to focus on your partner\'s eyes, mouth, and eyebrows to pick up subtle cues. Body language speaks the loudest in a conversation.Words like "enjoy," "awesome," and "ecstatic" are strong indicators of positive emotion.',
+      ];
+    } else if (diff <= 0.5) {
+      return [
+        "You're have a great grasp of your partner's emotional situation!",
+        "Try to focus on your partner's eyes, mouth, and eyebrows to pick up subtle cues. Body language speaks the loudest in a conversation.",
+      ];
+    } else if (diff <= 1) {
+      return [
+        "Nice job! Every person you meet will be different - practice makes perfect!",
+        "Listen to your partner when he/she talks. They are telling you more than just words!",
+      ];
+    }
+    return [
+      "If possible, try to put yourself in your partner's situation - how does that make you feel?",
+      'Words like "fine," "sure," and "okay" are useful in leveling your\'s and your partner\'s emotions',
+    ];
+  }
+
+  getScoreDescription(score) {
+    if (score <= -0.5) {
+      return [
+        "You are often at odds with your partner's emotions.",
+        "Possible causes are lack of attention and/or eye contact, negative language, or ignoring your partner.",
+        "Example: Your partner is frowning and visibly upset. Your response is to downplay their situation or aggravate their emotional state.",
+      ];
+    } else if (score <= -0.2) {
+      return [
+        "There are subtle differences between you and your partner's emotions.",
+        'Possible causes are using mild words such as "like," "sure," and "okay."',
+        "Example: Your partner is ecstatic about a recent event. Your response is unengaging and does not match his/her intensity.",
+      ];
+    } else if (score <= 0.2) {
+      return [
+        "You have a great understanding of your partner's emotions. Keep doing what you're doing!",
+        "A non-zero score could simply be a mathematical error. No need to worry - we need practice too!",
+        "Example: You and your partner are on the same level. Think of your facial expression and theirs - they probably match!",
+      ];
+    } else if (score <= 0.5) {
+      return [
+        "There are subtle differences between you and your partner's emotions.",
+        'Possible causes are using mild words such as "like," "sure," and "okay."',
+        "Example: Your partner is worried about personal matters. Your response is unengaging and uninviting.",
+      ];
+    }
+    return [
+      "You are often at odds with your partner's emotions.",
+      "Possible causes are lack of attention and/or eye contact, overly positive language, or ignoring your partner.",
+      "Example: Your partner is frowning and visibly upset. Your response is to laugh at their situation or aggravate their emotional state.",
+    ];
+  }
 
   render() {
-
     return (
       <div>
         <div className={styles.width}>
@@ -19,22 +91,26 @@ class ResultsView extends Component {
 
             <Grid columns={2} relaxed="very" className={styles.columns}>
               <Grid.Column>
-                <Statistic label="Your Sentiment Score" value="3.1" />
-                {/* <h1 style={{ fontSize: "35px" }}>3.1</h1>
-                <h3>Your Sentiment Score</h3> */}
-                <p>What is “Your Sentiment Score?”</p>
-                <p>What does this score mean?</p>
-                <p>What are possible causes of this score?</p>
-                <p>Examples?</p>
+                <Statistic
+                  label="Your Sentiment Score"
+                  value={this.state.userScore}
+                />
+                {this.getScoreDescription(this.state.userScore).map(
+                  (line, i) => {
+                    return <p key={i}>{line}</p>;
+                  }
+                )}
               </Grid.Column>
               <Grid.Column>
-                <Statistic label="Sim's Sentiment Score" value="7.7" />
-                {/* <h1 style={{ fontSize: "35px" }}>7.7</h1>
-                <h3>Sim's Sentiment Score</h3> */}
-                <p>What is “Your Sentiment Score?”</p>
-                <p>What does this score mean?</p>
-                <p>What are possible causes of this score?</p>
-                <p>Examples?</p>
+                <Statistic
+                  label="Sim's Sentiment Score"
+                  value={this.state.simScore}
+                />
+                {this.getScoreDescription(this.state.simScore).map(
+                  (line, i) => {
+                    return <p key={i}>{line}</p>;
+                  }
+                )}
               </Grid.Column>
             </Grid>
           </Message>
@@ -44,7 +120,9 @@ class ResultsView extends Component {
             info
             icon="lightbulb outline"
             header="Care for a few tips and takeaways?"
-            list={["Tip number 1", "Tip number 2"]}
+            list={this.tipsAndTakeaways(
+              parseFloat(this.state.userScore) - parseFloat(this.state.simScore)
+            )}
           />
         </div>
       </div>
