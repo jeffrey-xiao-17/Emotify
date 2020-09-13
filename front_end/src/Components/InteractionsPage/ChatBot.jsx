46b805ea75c835afa3fb46ca3370aabbbef3aab5
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-09-12 22:17:29 -0400

    added reaction to text

--- front_end/src/Components/InteractionsPage/ChatBot.jsx
+++ front_end/src/Components/InteractionsPage/ChatBot.jsx
@@ -5,13 +5,22 @@ import Avatar from "avataaars";
 
 class ChatBot extends Component {
   render() {
+    const expressions =
+      Math.abs(this.props.botSentiment - this.props.userSentiment) > 0.1
+        ? computeOptions(
+            1.5 * this.props.botSentiment + 0.5 * this.props.userSentiment
+          )
+        : {
+            eye: this.props.bot.eyeType,
+            eyebrow: this.props.bot.eyebrowType,
+            mouth: this.props.bot.mouthType,
+          };
+
     return (
       <div className={styles.window}>
         <Avatar
-          // style={{ width: "100px", height: "100px" }}
           className={cx("ui image", styles.avatar)}
           style={{
-            // maxWidth: "75vh",
             maxHeight: "75vh",
             height: "100vh",
             width: "100vh",
@@ -24,16 +33,12 @@ class ChatBot extends Component {
           facialHairType={this.props.bot.facialHairType}
           clotheType={this.props.bot.clotheType}
           clotheColor={this.props.bot.clotheColor}
-          eyeType={this.props.bot.eyeType}
-          eyebrowType={this.props.bot.eyebrowType}
-          mouthType={this.props.bot.mouthType}
           skinColor={this.props.bot.skinColor}
+          // The following three can change based on sentiment and words
+          eyeType={expressions.eye}
+          eyebrowType={expressions.eyebrow}
+          mouthType={expressions.mouth}
         />
-        {/* <img
-          src="https://react.semantic-ui.com/images/wireframe/image.png"
-          className={cx("ui image", styles.avatar)}
-          alt="placeholder"
-        /> */}
       </div>
     );
   }
@@ -41,46 +46,86 @@ class ChatBot extends Component {
 
 export default ChatBot;
 const EYE_OPTIONS = [
-  "Close",
-  "Cry",
-  "Default",
-  // "Dizzy",
-  "EyeRoll",
-  "Happy",
-  "Hearts",
-  "Side",
-  "Squint",
-  "Surprised",
-  "Wink",
-  "WinkWacky",
+  "Cry", // -1
+  "Side", // -0.8
+  "EyeRoll", // -0.6
+  "Close", // -0.4
+  "Squint", // -0.2
+  "Default", // 0
+  "WinkWacky", // 0.2
+  "Wink", // 0.4
+  "Surprised", // 0.6
+  "Happy", // 0.8
+  "Hearts", // 1
 ];
 
 const EYEBROW_OPTIONS = [
   "Angry",
   "AngryNatural",
-  "Default",
-  "DefaultNatural",
-  "FlatNatural",
-  "RaisedExcited",
-  "RaisedExcitedNatural",
   "SadConcerned",
   "SadConcernedNatural",
-  "UnibrowNatural",
+  "Default",
+  "DefaultNatural",
+  "RaisedExcited",
+  "RaisedExcitedNatural",
   "UpDown",
   "UpDownNatural",
 ];
 
 const MOUTH_OPTIONS = [
-  "Concerned",
-  "Default",
-  "Disbelief",
-  "Eating",
-  "Grimace",
-  "Sad",
   "ScreamOpen",
+  "Concerned",
+  "Sad",
+  "Disbelief",
   "Serious",
+  "Default",
+  "Eating",
+  "Twinkle",
   "Smile",
   "Tongue",
-  "Twinkle",
-  "Vomit",
 ];
+
+function computeOptions(s) {
+  var index = -1;
+  for (var i = 0; i < MOUTH_OPTIONS.length; i++) {
+    if (s <= -0.9 + i * 0.2) {
+      index = i;
+      break;
+    }
+  }
+  index = index == -1 ? 9 : index;
+  var res = {
+    eye: 0,
+    eyebrow: 0,
+    mouth: 0,
+  };
+  if (index == 0) {
+    res.eye = EYE_OPTIONS[Math.round(Math.random(1))];
+    res.eyebrow = EYEBROW_OPTIONS[Math.round(Math.random(1))];
+    res.mouth = MOUTH_OPTIONS[Math.round(Math.random(1))];
+  } else if (index == 9) {
+    res.eye = EYE_OPTIONS[9 - Math.round(Math.random(1))];
+    res.eyebrow = EYEBROW_OPTIONS[9 - Math.round(Math.random(1))];
+    res.mouth = MOUTH_OPTIONS[9 - Math.round(Math.random(1))];
+  } else {
+    res.eye =
+      EYE_OPTIONS[
+        index +
+          Math.floor(Math.random() + 0.5) *
+            (Math.floor(Math.random() * 2) == 1 ? 1 : -1)
+      ];
+    res.eyebrow =
+      EYEBROW_OPTIONS[
+        index +
+          Math.floor(Math.random() + 0.5) *
+            (Math.floor(Math.random() * 2) == 1 ? 1 : -1)
+      ];
+    res.mouth =
+      MOUTH_OPTIONS[
+        index +
+          Math.floor(Math.random() + 0.5) *
+            (Math.floor(Math.random() * 2) == 1 ? 1 : -1)
+      ];
+  }
+  return res;
+}
