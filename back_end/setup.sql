cc8b253f2e14d2040968d3cb54770b4d8462b837
M00ny0z <1mannymunoz@gmail.com>
2020-09-12 18:30:30 -0700

    Added sim table and added sim_id column to interaction table

--- back_end/setup.sql
+++ back_end/setup.sql
@@ -19,6 +19,22 @@ CREATE TABLE interaction(
    user_score INT NOT NULL,
    topic      VARCHAR(200) NOT NULL,
    name       VARCHAR(100) NOT NULL,
+   sim_id     INT NOT NULL,
    date_made  DATE NOT NULL DEFAULT NOW(),
-   FOREIGN KEY (user_id) REFERENCES user(id)
+   FOREIGN KEY (user_id) REFERENCES user(id),
+   FOREIGN KEY (sim_id) REFERENCES sim(id)
+);
+
+CREATE TABLE sim(
+   id             INT NOT NULL AUTO_INCREMENT,
+   accessory_type VARCHAR(100) NOT NULL,
+   hair_color     VARCHAR(100) NOT NULL,
+   hat_color      VARCHAR(100) NOT NULL,
+   facial_hair    VARCHAR(100) NOT NULL,
+   clothe         VARCHAR(100) NOT NULL,
+   clothe_color   VARCHAR(100) NOT NULL,
+   eye_type       VARCHAR(100) NOT NULL DEFAULT "Default",
+   eyebrow_type   VARCHAR(100) NOT NULL DEFAULT "Default",
+   mouth_type     VARCHAR(100) NOT NULL DEFAULT "Default",
+   skin_color     VARCHAR(100) NOT NULL
 );
